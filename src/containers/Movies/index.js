b8997d75b8e002721041b62e9f0bff90112bd1d1
khushbosinghrecs <9918khush@gmail.com>
2024-06-12 18:33:19 +0530

    movie-list

--- src/containers/Movies/index.js
+++ src/containers/Movies/index.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
+
 import { Table } from "../../components/Table";
 import Button from "../../components/Button/Button";
-
-import { API_KEY, API_URL } from "../../url";
-
 import Modal from "../../components/Modal";
 import MovieDetail from "../../components/MovieDetail/MovieDetail";
 
+import { API_KEY, API_URL } from "../../url";
 import Eye from "../../assets/svg/eye.svg";
+
 import styles from "./styles.module.scss";
 
 const columns = [
@@ -18,92 +18,9 @@ const columns = [
   { header: "", accessor: "view" },
 ];
 
-const data = [
-  {
-    Title: "The Lego Movie",
-    Year: "2014",
-    imdbID: "tt1490017",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMTg4MDk1ODExN15BMl5BanBnXkFtZTgwNzIyNjg3MDE@._V1_SX300.jpg",
-  },
-  {
-    Title: "The Simpsons Movie",
-    Year: "2007",
-    imdbID: "tt0462538",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNjc4NmQyNGUtMDg4NS00ZTZkLWI3ODQtMGJmYThiYjQxNGRiXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
-  },
-  {
-    Title: "El Camino: A Breaking Bad Movie",
-    Year: "2019",
-    imdbID: "tt9243946",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNjk4MzVlM2UtZGM0ZC00M2M1LThkMWEtZjUyN2U2ZTc0NmM5XkEyXkFqcGdeQXVyOTAzMTc2MjA@._V1_SX300.jpg",
-  },
-  {
-    Title: "Scary Movie",
-    Year: "2000",
-    imdbID: "tt0175142",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMGEzZjdjMGQtZmYzZC00N2I4LThiY2QtNWY5ZmQ3M2ExZmM4XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-  },
-  {
-    Title: "The Super Mario Bros. Movie",
-    Year: "2023",
-    imdbID: "tt6718170",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BOTJhNzlmNzctNTU5Yy00N2YwLThhMjQtZDM0YjEzN2Y0ZjNhXkEyXkFqcGdeQXVyMTEwMTQ4MzU5._V1_SX300.jpg",
-  },
-  {
-    Title: "Bee Movie",
-    Year: "2007",
-    imdbID: "tt0389790",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjE1MDYxOTA4MF5BMl5BanBnXkFtZTcwMDE0MDUzMw@@._V1_SX300.jpg",
-  },
-  {
-    Title: "Scary Movie 2",
-    Year: "2001",
-    imdbID: "tt0257106",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMzQxYjU1OTUtYjRiOC00NDg2LWI4MWUtZGU5YzdkYTcwNTBlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-  },
-  {
-    Title: "The Lego Batman Movie",
-    Year: "2017",
-    imdbID: "tt4116284",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMTcyNTEyOTY0M15BMl5BanBnXkFtZTgwOTAyNzU3MDI@._V1_SX300.jpg",
-  },
-  {
-    Title: "Scary Movie 3",
-    Year: "2003",
-    imdbID: "tt0306047",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNDE2NTIyMjg2OF5BMl5BanBnXkFtZTYwNDEyMTg3._V1_SX300.jpg",
-  },
-  {
-    Title: "Scary Movie 4",
-    Year: "2006",
-    imdbID: "tt0362120",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmFkMzc2NTctN2U1Ni00MzE5LWJmMzMtYWQ4NjQyY2MzYmM1XkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg",
-  },
-];
-
 const Movie = () => {
   const [searchValue, setSearchValue] = useState("movie");
-  const [movies, setMovies] = useState(data);
+  const [movies, setMovies] = useState([]);
   const [movieTitle, setMovieTitle] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [sortDirection, setSortDirection] = useState("asc");
@@ -203,7 +120,7 @@ const Movie = () => {
         {movies && movies?.length > 0 ? (
           <Table columns={columns} data={data2} />
         ) : (
-          <p>No data</p>
+          <p>loading.....</p>
         )}
       </div>
       <Modal
